d66491dd9baa1ff7b1abf7158daeef4462b40679
MannixWong <MannixWong@DESKTOP-9ATENED>
2021-04-12 20:48:36 +0800

    更改绘图函数中的y,显示为经过幅值平均和归一化后的值

--- DataProcessing.m
+++ DataProcessing.m
@@ -1,7 +1,7 @@
 
 clear;close all;clc
 M=36
-filen='C:\Users\MannixWong\Desktop\data\4k-3.dat';
+filen='C:\Users\MannixWong\Desktop\data\20210412_16_57_12.dat';
 height=1000000;
 fid=fopen(filen,'r');%读出
 img=fread(fid,[M,height],'uint16');%将数据读到1*40000的数组中，指定源数据为uint16类
@@ -19,13 +19,13 @@ for i =1:m
         end
     end
 end
-Startpoint=1500;
+Startpoint=3000;
 Sample=1000;
 x=1:1:Sample;
 for i=1:M
 y(i,:)=img(i ,Startpoint:Startpoint+Sample-1);
-% y(i,:)=y(i,:)-(max(y(i,:))+min(y(i,:)))/2;
-% [y(i,:),PS]=mapminmax(y(i,:));
+y(i,:)=y(i,:)-(max(y(i,:))+min(y(i,:)))/2;
+[y(i,:),PS]=mapminmax(y(i,:));
 end
 
 %% 相关性分析
@@ -55,7 +55,8 @@ save(string,'X')
 figure('Name','1-8通道')
 for i=1:8
     subplot(4,2,i)
-    plot(x,img(i,Startpoint:Startpoint+Sample-1))
+%     plot(x,img(i,Startpoint:Startpoint+Sample-1))
+      plot(x,y(i,:))
 %     y_val=get(gca,'YTick');   %为了获得y轴句柄
 %     y_str=num2str(y_val');    %为了将数字转换为字符数组
 %     set(gca,'YTickLabel',y_str);    %显示
@@ -68,7 +69,8 @@ end
 figure('Name','9-16通道')
 for i=9:16
     subplot(4,2,i-8)
-    plot(x,img(i,Startpoint:Startpoint+Sample-1))
+%     plot(x,img(i,Startpoint:Startpoint+Sample-1))
+    plot(x,y(i,:))
 %     y_val=get(gca,'YTick');   %为了获得y轴句柄
 %     y_str=num2str(y_val');    %为了将数字转换为字符数组
 %     set(gca,'YTickLabel',y_str);    %显示
@@ -78,42 +80,45 @@ for i=9:16
     title(titlestring,'fontname','黑体','FontSize',12);
     grid on
 end
-% figure('Name','17-24通道')
-% for i=17:24
-%     subplot(4,2,i-16)
+figure('Name','17-24通道')
+for i=17:24
+    subplot(4,2,i-16)
 %     plot(x,img(i,Startpoint:Startpoint+Sample-1))
-% %     y_val=get(gca,'YTick');   %为了获得y轴句柄
-% %     y_str=num2str(y_val');    %为了将数字转换为字符数组
-% %     set(gca,'YTickLabel',y_str);    %显示
-%     titlestring=strcat('第',num2str(i),'通道');
-%     xlabel('采样点','FontSize',12);
-%     ylabel('幅值','FontSize',12);
-%     title(titlestring,'fontname','黑体','FontSize',12);
-%     grid on
-% end
-% figure('Name','25-32通道')
-% for i=25:32
-%     subplot(4,2,i-24)
+plot(x,y(i,:))
+%     y_val=get(gca,'YTick');   %为了获得y轴句柄
+%     y_str=num2str(y_val');    %为了将数字转换为字符数组
+%     set(gca,'YTickLabel',y_str);    %显示
+    titlestring=strcat('第',num2str(i),'通道');
+    xlabel('采样点','FontSize',12);
+    ylabel('幅值','FontSize',12);
+    title(titlestring,'fontname','黑体','FontSize',12);
+    grid on
+end
+figure('Name','25-32通道')
+for i=25:32
+    subplot(4,2,i-24)
 %     plot(x,img(i,Startpoint:Startpoint+Sample-1))
-% %     y_val=get(gca,'YTick');   %为了获得y轴句柄
-% %     y_str=num2str(y_val');    %为了将数字转换为字符数组
-% %     set(gca,'YTickLabel',y_str);    %显示
-%     titlestring=strcat('第',num2str(i),'通道');
-%     xlabel('采样点','FontSize',12);
-%     ylabel('幅值','FontSize',12);
-%     title(titlestring,'fontname','黑体','FontSize',12);
-%     grid on
-% end
-% figure('Name','33-36通道')
-% for i=33:36
-%     subplot(2,2,i-32)
+    plot(x,y(i,:))
+%     y_val=get(gca,'YTick');   %为了获得y轴句柄
+%     y_str=num2str(y_val');    %为了将数字转换为字符数组
+%     set(gca,'YTickLabel',y_str);    %显示
+    titlestring=strcat('第',num2str(i),'通道');
+    xlabel('采样点','FontSize',12);
+    ylabel('幅值','FontSize',12);
+    title(titlestring,'fontname','黑体','FontSize',12);
+    grid on
+end
+figure('Name','33-36通道')
+for i=33:36
+    subplot(2,2,i-32)
 %     plot(x,img(i,Startpoint:Startpoint+Sample-1))
-% %     y_val=get(gca,'YTick');   %为了获得y轴句柄
-% %     y_str=num2str(y_val');    %为了将数字转换为字符数组
-% %     set(gca,'YTickLabel',y_str);    %显示
-%     titlestring=strcat('第',num2str(i),'通道');
-%     xlabel('采样点','FontSize',12);
-%     ylabel('幅值','FontSize',12);
-%     title(titlestring,'fontname','黑体','FontSize',12);
-%     grid on
-% end
+plot(x,y(i,:))
+%     y_val=get(gca,'YTick');   %为了获得y轴句柄
+%     y_str=num2str(y_val');    %为了将数字转换为字符数组
+%     set(gca,'YTickLabel',y_str);    %显示
+    titlestring=strcat('第',num2str(i),'通道');
+    xlabel('采样点','FontSize',12);
+    ylabel('幅值','FontSize',12);
+    title(titlestring,'fontname','黑体','FontSize',12);
+    grid on
+end
